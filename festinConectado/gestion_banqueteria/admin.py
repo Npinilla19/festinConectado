949cb07f968903915a332836e72cc63cab2cefbe
Nicole <npinilla89@gmail.com>
2024-11-20 20:01:28 -0300

    se crea modelo, y se agregan a admin

---
+++ festinConectado/gestion_banqueteria/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Usuario, Banqueteria, Cotizacion
+
+admin.site.register(Usuario)
+admin.site.register(Banqueteria)
+admin.site.register(Cotizacion)
---
+++ festinConectado/gestion_banqueteria/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+class Usuario(models.Model):
+    TIPO_USUARIO = (
+        ('clientes', 'Clientes'), 
+        ('proveedor', 'Proveedor'),
+    )
+
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    direccion = models.CharField(max_length=250)
+    telefono = models.CharField(max_length=20)
+    correo = models.EmailField(unique=True)
+    tipo_usuario = models.CharField(max_length=10, choices=TIPO_USUARIO)
+
+    def __str__(self):
+        return f'{self.nombre} {self.apellido}'
+
+class Banqueteria(models.Model):
+    TIPO_EVENTO = (
+        ('matrimonio', 'Matrimonio'),
+        ('cumpleaños', 'Cumpleaños'),
+        ('corporativo', 'Corporativo'),
+        ('licenciatura', 'Licenciatura'),
+        ('otro', 'Otro'),
+    )
+
+    proveedor = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    nombre_servicio = models.CharField(max_length=200)
+    descripcion = models.TextField()
+    direccion = models.CharField(max_length=200)
+    comuna = models.CharField(max_length=100)
+    tipo_evento = models.CharField(max_length=50)
+    precio_estimado = models.FloatField()
+    disponibilidad = models.CharField(max_length=200)
+
+def __str__(self):
+    return self.nombre_servicio
+ 
+class Cotizacion(models.Model):
+    cliente = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    banqueteria = models.ForeignKey(Banqueteria, on_delete=models.CASCADE)
+    fecha_evento = models.DateTimeField(auto_now_add=True)
+    mensaje = models.TextField()
+
+    def __str__(self):
+        return f'Cotización para {self.banqueteria.nombre_servicio} por {self.cliente.nombre}'
