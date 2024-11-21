a97a63443c45f1945f384aaa1f121297a2489d93
Nicole <npinilla89@gmail.com>
2024-11-21 17:53:45 -0300

    se comienza a realizar las vistar de registro

--- festinConectado/festinConectado/settings.py
+++ festinConectado/festinConectado/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'festinConectado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['gestion_banqueteria/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- festinConectado/gestion_banqueteria/views.py
+++ festinConectado/gestion_banqueteria/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib import messages
 
-# Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Cuenta creada exitosamente para {form.cleaned_data["username"]}!')
+            return redirect('login')
+
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/register.html', {'form': form})
